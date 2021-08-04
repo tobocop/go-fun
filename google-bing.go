9b2f3ffab9e91573334bd45a7f8d6f76a295706c
Toby Rumans <tobyrumans@Tobys-Air.localdomain>
2021-08-04 16:31:41 -0600

    slightly better

--- google-bing.go
+++ google-bing.go
@@ -2,14 +2,16 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
+	"os"
 )
 
 func main() {
+	searchTerm := os.Args[1:]
 	bingResults := make(chan *http.Response)
 	googleResults := make(chan *http.Response)
 
-	searchTerm := "some-search"
 	go func() {
 		bing, _ := http.Get(fmt.Sprintf("https://www.bing.com/search?q=%s", searchTerm))
 		bingResults <- bing
@@ -21,12 +23,14 @@ func main() {
 
 	select {
 	case res := <-bingResults:
-		fmt.Println(res)
-		fmt.Println("Bing won")
+		outputWinner("Bing", res)
 	case res := <-googleResults:
-		fmt.Println(res)
-		fmt.Println("Google won")
+		outputWinner("Google", res)
 	}
 }
 
-
+func outputWinner(winner string, res *http.Response) {
+	body, _ := ioutil.ReadAll(res.Body)
+	fmt.Println(string(body[:200]))
+	fmt.Printf("\n%s won\n", winner)
+}
